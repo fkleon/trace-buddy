4a80cb93c39a312aa8ef66a44d391ca9bab312d0
Frederik Leonhardt <frederik.leonhardt@googlemail.com>
2013-01-09 20:01:48 +0100

    Web interface for raytracer.

--- web/TraceBuddy.dart
+++ web/TraceBuddy.dart
@@ -1,56 +1,127 @@
 import 'dart:html';
+import 'dart:isolate';
+
+import 'package:vector_math/vector_math_console.dart';
+
 import '../rt/renderer.dart';
 import '../rt/samplers.dart';
 import '../rt/algebra.dart';
 import '../rt/ray.dart';
 
-import 'package:vector_math/vector_math_console.dart';
-
-int xRes, yRes;
+var view;
 
 void main() {
-  xRes = 400;
-  yRes = 300;
-  
-  CanvasElement imageCanvas = query("#image");
-  imageCanvas.width = xRes;
-  imageCanvas.height = yRes;
-  
-  Sampler sampler = new DefaultSampler();
-  Camera camera = new PerspectiveCamera(
-      new Point3D.zero(),
-      new vec3.raw(1,0,0),
-      new vec3.raw(0,1,0),
-      60,
-      new vec2.raw(xRes, yRes));
-  
-  Collection<Primitive> primitives = [new InfinitePlane(new Point3D(0,-2,0),new vec3.raw(0, 1, 0)),
-                                      new Sphere(new Point3D(10,0,0),2)];
-  
-  Scene scene = new Scene(primitives);
-  
-  Renderer r = new Renderer(scene, sampler, camera);
-  OutputMatrix om = r.render();
-  
-  AsciiDumper.dumpAsciiRGB(om);
-  
-  writeToCanvas2d(om, imageCanvas);
+  print('init main');
+  var rc = new RenderController();
+  view = new TraceBuddyView(rc);
+  rc.view = view;
 }
 
-writeToCanvas2d(OutputMatrix om, CanvasElement canvas) {
-  ImageData id = canvas.context2d.createImageData(om.columns, om.rows);
+class RenderController {
+  // view
+  TraceBuddyView view;
   
-  int i = 0;
-  for (vec3 color in om.getSerialized()) {
-    // set RGBA
-    id.data[i++] = color[0].toInt()*255;
-    id.data[i++] = color[1].toInt()*255;
-    id.data[i++] = color[2].toInt()*255;
-    id.data[i++] = 255;
+  // rendering stuff
+  Scene scene;
+  Camera camera;
+  Sampler sampler;
+  Renderer renderer;
+  OutputMatrix om;
+  
+  RenderController() {
+    // initialize renderer
+    createRenderer();
   }
   
-  canvas.context2d.putImageData(id, 0, 0);
+  get primitives => scene.nonIdxPrimitives;
+  
+  void renderSceneIsolate() {
+    port.receive((_,__) {
+      renderScene();
+      print('done');
+    });
+  }
+  
+  void renderScene() {
+    if (view == null) {
+      throw new ArgumentError('Did not define a view.'); //TODO remove this non-sense
+    }
+    createRenderer();
+    
+    this.view.renderInfo = 'rendering..';
+    
+    // time the process
+    var timer = new Stopwatch();
+    timer.start();
+    
+    // render the scene
+    om = renderer.render();
+    //AsciiDumper.dumpAsciiRGB(om);
+    
+    num timeForRender = timer.elapsedMilliseconds;
+    
+    // write the image to canvas element
+    //TODO use view
+//    writeToCanvas2d(om, imageCanvasElement);
+    
+    this.view.renderInfo = 'Done. Rendered in ${timeForRender/1000.0} s.';
+  }
+  
+  void createRenderer() {
+    // load scene
+    if (scene == null) {
+      Collection<Primitive> primitives = [new InfinitePlane(new Point3D(0,-2,0),new vec3.raw(0, 1, 0)),
+                                          new CartesianCoordinateSystem(),
+                                          new Sphere(new Point3D(10,0,0),2),
+                                          new Sphere(new Point3D(-4,-1,1),0.2)];
+      scene = new Scene(primitives);
+    }
+    
+    // add coordinate system
+    
+    //TODO this is a ugly hack
+    scene.removeCartesianCoordSystems();
+    if (view == null ? true : view.renderCoords) {
+      scene.add(new CartesianCoordinateSystem());
+    }
+    
+    // load sampler
+    if (sampler == null) {
+      //TODO apply quality settings?
+      sampler = new DefaultSampler();
+    }
+    
+    // load camera
+    if (camera == null) {
+      Point3D cameraOrigin = view == null ? new Point3D(-5,2,-5) : this.view.cameraOrigin;
+      vec2 res = view == null ? new vec2.raw(300,200) : this.view.res;
+      
+      camera = new PerspectiveCamera.lookAt(
+          cameraOrigin,
+          new Point3D(0,0,0),
+          new vec3.raw(0,1,0),
+          60,
+          res);
+  //    camera = new PerspectiveCamera(
+  //        new Point3D(-10,2,-1),
+  //        new vec3.raw(1,-0.5,0),
+  //        new vec3.raw(0,1,0),
+  //        60,
+  //        new vec2.raw(xRes, yRes));
+      
+//      camera = new PerspectiveCamera.lookAt(
+//          new Point3D(-5,2,-5),
+//          new Point3D(0,0,0),
+//          new vec3.raw(0,1,0),
+//          60,
+//          new vec2.raw(xRes, yRes));
+    }
+    
+    // create renderer
+    renderer = new Renderer(scene, sampler, camera);
+  }
 }
+
 
 class AsciiDumper {
   static void dumpAsciiRGB(OutputMatrix om) {
@@ -75,4 +146,112 @@ class AsciiDumper {
   static bool isRed(vec3 rgb) => rgb.x == 1 && rgb.y == 0 && rgb.z == 0;
   static bool isGreen(vec3 rgb) => rgb.x == 0 && rgb.y == 1 && rgb.z == 0;
   static bool isBlue(vec3 rgb) => rgb.x == 0 && rgb.y == 0 && rgb.z == 1;
+}
+
+
+class TraceBuddyView {
+  RenderController rc;
+  //get rc => _rc == null ? new RenderController(this) : _rc;
+  
+  // Render information
+  String renderInfo;
+  
+  // Scene information
+  get sceneInformation => '${primitives.length} primitives in scene.';
+  get primitives => rc.primitives;
+
+  // Camera properties
+  String xResStr, yResStr;
+  String xOriginStr, yOriginStr, zOriginStr;
+  
+  // Rendering properties
+  bool renderCoords;
+  
+  get imageCanvas => query('#imageCanvas');
+  
+  TraceBuddyView(RenderController this.rc) {
+    renderInfo = '';
+    xResStr = '400';
+    yResStr = '300';
+    xOriginStr = zOriginStr = '-5.0';
+    yOriginStr = '5.0';
+    renderCoords = true;
+    imageCanvas.width = xRes;
+    imageCanvas.height = yRes;
+  }
+ 
+ int get xRes => _parseInt(xResStr);
+ int get yRes => _parseInt(yResStr);
+ vec2 get res => new vec2.raw(xRes, yRes);
+ 
+ Point3D get cameraOrigin =>
+      new Point3D(_parseDouble(xOriginStr),
+                  _parseDouble(yOriginStr),
+                  _parseDouble(zOriginStr));
+ 
+ void removePrimitive(Event e) {
+   Element target = (e.target as Element);
+   int elementId = int.parse(target.attributes['data-item-id']);
+   rc.scene.remove(elementId);
+   e.preventDefault();
+ }
+ 
+ void render() {
+   // TODO use isolate
+   // TODO write method
+   rc.camera = null; //TODO find nifty solution
+   
+//   SendPort renderer = spawnFunction(rc.renderSceneIsolate);
+//   renderer.send('test');
+   
+   rc.renderScene();
+   //AsciiDumper.dumpAsciiRGB(rc.om);
+   
+   drawImage(rc.om);
+ }
+ 
+ void drawImage(OutputMatrix om) {
+   var timer = new Stopwatch();
+   timer.start();
+   _writeToCanvas2d(om, imageCanvas);
+   this.renderInfo = '${renderInfo} Drawn in ${timer.elapsedMilliseconds/1000} s.';
+ }
+ 
+ void _writeToCanvas2d(OutputMatrix om, CanvasElement canvas) {
+   // make sure canvas is big enough
+   canvas.width = om.columns;
+   canvas.height = om.rows;
+   
+   // convert OM infomration to canvas information
+   ImageData id = canvas.context2d.createImageData(om.columns, om.rows);
+   
+   int i = 0;
+   for (vec3 color in om.getSerialized()) {
+     // set RGBA
+     id.data[i++] = (color[0]*255).toInt();
+     id.data[i++] = (color[1]*255).toInt();
+     id.data[i++] = (color[2]*255).toInt();
+     id.data[i++] = 255;
+   }
+   
+   canvas.context2d.putImageData(id, 0, 0);
+ }
+ 
+ int _parseInt(String value) {
+   try {
+     return int.parse(value);
+   } on FormatException catch (e) {
+     print('Illegal integer: $value ($e)');
+     return 0;
+   }
+ }
+ 
+ double _parseDouble(String value) {
+   try {
+     return double.parse(value);
+   } on FormatException catch (e) {
+     print('Illegal double: $value ($e)');
+     return 0.0;
+   }
+ }
 }
