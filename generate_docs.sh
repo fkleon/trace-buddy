c06271e70e8ba41b42cc91a2c4c46763528e6d62
Frederik Leonhardt <frederik.leonhardt@googlemail.com>
2013-01-23 10:04:18 +0100

    Add math_browser library to docs exclude list.

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Path to dartdoc executable
 dartdoc="/home/freddy/Downloads/dart/dart-sdk/bin/dartdoc"
 
-$dartdoc --no-show-private --no-code --out ./docs --exclude-lib dd_entry,unittest,mock,matcher,vector_math_console,vector_math_html -v "dartdoc_entry_point.dart"
+# Creates dart doc, excludes third party libraries from drocumentation
+$dartdoc --no-show-private --no-code --out ./docs --exclude-lib dd_entry,unittest,mock,matcher,vector_math_browser,vector_math_console,vector_math_html -v "dartdoc_entry_point.dart"
