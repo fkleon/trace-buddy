06605c5449c7f53248ca39192a714224e438d10d
Frederik Leonhardt <frederik.leonhardt@googlemail.com>
2013-01-25 01:32:31 +0100

    Migrate and fix unit test.

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -14,15 +14,15 @@ class ParserTests extends TestSet {
   void initTests() {
     pars = new Parser();
     lex = new Lexer();
+
     inputString1 = "x + 2";
-    inputString3 = "log(10)(100)";
-    
-    tokenStream1 = new List<Token>();
-    tokenStream1.add(new Token("x", TokenType.VAR));
-    tokenStream1.add(new Token("+", TokenType.PLUS));
-    tokenStream1.add(new Token("2", TokenType.VAL));
-    
     inputString2 = "x * 2^2.5 * log(10)(100)";
+    inputString3 = "log(10)(100)";
+
+    tokenStream1 = [new Token("x", TokenType.VAR),
+                    new Token("+", TokenType.PLUS),
+                    new Token("2", TokenType.VAL)];
+
     tokenStream2 = [new Token("x", TokenType.VAR),
                     new Token("*", TokenType.TIMES),
                     new Token("2", TokenType.VAL),
@@ -36,7 +36,7 @@ class ParserTests extends TestSet {
                     new Token("(", TokenType.LBRACE),
                     new Token("100", TokenType.VAL),
                     new Token(")", TokenType.RBRACE)];
-    
+
     upnTokenStream2 = [new Token("x", TokenType.VAR),
                     new Token("*", TokenType.TIMES),
                     new Token("2", TokenType.VAL),
@@ -50,54 +50,51 @@ class ParserTests extends TestSet {
                     new Token("(", TokenType.LBRACE),
                     new Token("100", TokenType.VAL),
                     new Token(")", TokenType.RBRACE)];
-    
-    tokenStream3 = [new Token("log", TokenType.LOG),
-                    new Token("10", TokenType.VAL),
-                    new Token("100", TokenType.VAL)];
-  }
-  
-  
 
-  
+    tokenStream3 = [new Token("log", TokenType.LOG),
+                    new Token("(", TokenType.LBRACE),
+                    new Token("10", TokenType.VAL),
+                    new Token(")", TokenType.RBRACE),
+                    new Token("(", TokenType.LBRACE),
+                    new Token("100", TokenType.VAL),
+                    new Token(")", TokenType.RBRACE)];
+  }
 
   /*
    *  Tests and variables.
    */
-
   Parser pars;
   Lexer lex;
   String inputString1;
   String inputString2;
   String inputString3;
-  
+
   List<Token> tokenStream1;
   List<Token> tokenStream2;
   List<Token> tokenStream3;
 
   List<Token> upnTokenStream1;
   List<Token> upnTokenStream2;
-  
-  
-  void parserTokenTest(){
+
+
+  void parserTokenTest() {
     List<Token> stream1 = lex.createTokenStream(inputString1);
     List<Token> stream2 = lex.createTokenStream(inputString2);
     List<Token> stream3 = lex.createTokenStream(inputString3);
 
     List<Token> upnStream1 = lex.shuntingYard(stream1);
     List<Token> upnStream2 = lex.shuntingYard(stream2);
-   
-    
+
     expect(stream1, orderedEquals(tokenStream1));
     expect(stream1, isNot(orderedEquals(tokenStream2)));
     expect(stream2, orderedEquals(tokenStream2));
     expect(stream2, isNot(orderedEquals(tokenStream1)));
     expect(stream3, orderedEquals(tokenStream3));
-
-  }
-  
-void parserExpressionTest(){
-    
   }
 
-  
+  void parserExpressionTest() {
+    throw new UnimplementedError();
+  }
+
+
 }
